4339bf05075167ad6df9183b35e7fd4e40f0783b
FelixPolkanov <106100262+FelixPolkanov@users.noreply.github.com>
2023-01-04 18:50:07 +0200

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+import { ContactForm } from './ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+
+import { nanoid } from 'nanoid';
+import { Title, TitleContacts, Wrapper } from './App.styled';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  handleInputChange = e => {
+    this.setState({
+      [e.currentTarget.name]: e.currentTarget.value,
+    });
+  };
+
+  addContact = data => {
+    const { name, number } = data;
+    if (this.checkDoubleContact(data)) {
+      alert(`${name} is already in your contacts!`);
+      return;
+    }
+    const newContact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+
+    this.setState(prevState => ({
+      contacts: [newContact, ...prevState.contacts],
+    }));
+  };
+
+  componentDidUpdate(_, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('lsContacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  componentDidMount() {
+    const lsContact = localStorage.getItem('lsContacts');
+    const lsContactParsed = JSON.parse(lsContact);
+    if (lsContactParsed) {
+      this.setState({ contacts: lsContactParsed });
+    }
+  }
+
+  checkDoubleContact = inputData => {
+    return this.state.contacts.find(contact => contact.name === inputData.name);
+  };
+
+  getVisibleContacts = () => {
+    const { contacts, filter } = this.state;
+    const normalized = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalized)
+    );
+  };
+
+  deleteContact = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(el => el.id !== id),
+    }));
+  };
+
+  render() {
+    const visibleContacts = this.getVisibleContacts();
+
+    return (
+      <Wrapper>
+        <Title>Phonebook</Title>
+        <ContactForm onSubmit={this.addContact} />
+        <TitleContacts>Contacts:</TitleContacts>
+        <Filter value={this.state.filter} onFilter={this.handleInputChange} />
+        <ContactList
+          contacts={visibleContacts}
+          deleteContact={this.deleteContact}
+        />
+      </Wrapper>
+    );
+  }
+}
